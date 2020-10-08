f2830193448b94fd844cba58f257e87f2dac7456
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2020-10-08 14:23:24 -0500

    updated working code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,121 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userResponses, userInfo) {
 
-`;
+  // Generate Table of Contents conditionally based on userResponses
+  let tableContents = `## Table of Contents`;
+
+  if (userResponses.installation !== '') { tableContents += `
+  * [Installation](#installation)` };
+
+  if (userResponses.usage !== '') { tableContents += `
+  * [Usage](#usage)` };
+
+  if (userResponses.contributing !== '') { tableContents += `
+  * [Contributing](#contributing)` };
+
+  if (userResponses.tests !== '') { tableContents += `
+  * [Tests](#tests)` };
+
+
+  // Generate markdown for the README
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  
+  ## Description 
+  
+  ${userResponses.description}
+  `
+
+  // Add Table of Contents to markdown
+  draftMarkdown += tableContents;
+ 
+  // Add License section since License is required to Table of Contents
+  draftMarkdown += `
+  * [License](#license)`;
+  
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  ## Installation
+  
+  ${userResponses.installation}`
+  };
+  
+  // Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  ## Usage 
+    
+  ${userResponses.usage}`
+  };
+  
+  
+  // Contributing section
+  if (userResponses.contributing !== '') {
+  `
+  
+  ## Contributing
+    
+  ${userResponses.contributing}`
+  };
+  
+
+  // Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  
+  ${userResponses.tests}`
+  };
+
+
+  // License section is required
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+
+  // Questions / About Developer section
+  let draftDev = 
+  `
+  ---
+  
+  ## Questions?
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+  // If GitHub email is not null, add to Developer section
+  if (userInfo.email !== null) {
+  
+  draftDev +=
+  `
+  Email: ${userInfo.email}
+  `};
+
+  // Add developer section to markdown
+  draftMarkdown += draftDev;
+
+  // Return markdown
+  return draftMarkdown;
+  
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,16 +1,132 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+            }
+        },
+        {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+        },
+        {
+            type: 'input',
+            message: 'Include GitHub Repo',
+            name: 'username',
+            default: 'ktinj',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+                }
+            },
+            {
+                type: 'input',
+                message: "Describe the steps required for installation.",
+                name: 'installation'
+            },
+            {
+                type: 'input',
+                message: "Provide instructions and examples of usage.",
+                name: 'usage'
+            },
+            {
+                type: 'input',
+                message: "Provide guidelines for contributors.",
+                name: 'contributing'
+            },
+            {
+                type: 'input',
+                message: "Provide any tests written for the application.",
+                name: 'tests'
+            },
+            {
+                type: 'input',
+                message: "Enter a license for your project.",
+                name: 'license'
+            },
+            {
+                type: 'input',
+                message: "Questions?",
+                name: 'questions'
+            }
 ];
+
+ // call GitHub api
+ const axios = require('axios');
+
+const api = {
+  async getUser(userResponses) {
+    try { let response = await axios
+    
+        .get(`https://api.github.com/users/${userResponses.username}`);
+        return response.data;
+
+      } catch (error) {
+        console.log(error);
+      }
+  }
+};
+
+module.exports = api;
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, err => {
+    if (err) {
+        return console.log(err);
+      }
+    
+      console.log("Your README.md file has been generated")
+  });
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
-function init() {
+async function init() {
 
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+
+         // Call GitHub api for user info
+         const userInfo = await api.getUser(userResponses);
+         console.log("Your GitHub user info: ", userInfo);
+     
+         // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+         console.log("Generating your README")
+         const markdown = generateMarkdown(userResponses, userInfo);
+         console.log(markdown);
+     
+         // Write markdown to file
+         await writeFileAsync('ExampleREADME.md', markdown);
+ 
+     } catch (error) {
+         console.log(error);
+     }
+ };
 }
-
-// function call to initialize program
-init();
+init(); 
